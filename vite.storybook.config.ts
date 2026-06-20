0e982c15bb76d41c3d83154d7ec7d84e4522c6b6
Zertonixa <olpin9070@gmail.com>
2026-06-20 05:00:03 +0300

    refactor: organize application into layers

--- vite.storybook.config.ts
+++ vite.storybook.config.ts
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   resolve: { tsconfigPaths: true },
-  plugins: [tailwindcss()],
 })
